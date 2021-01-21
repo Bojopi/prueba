c6b2567a1d071cbbe81bc561ee40de3416302496
Bryana Ojopi Banegas <ojopibryana5@gmail.com>
2021-01-21 16:19:42 -0400

    se agregó bootstrap al proyecto

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 // const user = {
 //     firstName: 'Bryana',
